e5bd55d95aeaca655e876e0e5a6192b08fc02ce9
Manas Gupta <manasgupta1109@gmail.com>
2020-10-01 01:09:36 +0530

    Added ubuntu 20.04 cmake installation

---
+++ CMake/Ubuntu20.sh
@@ -0,0 +1,27 @@
+sudo apt-get update
+
+sudo apt-get install apt-transport-https ca-certificates gnupg software-properties-common wget -y
+
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
+
+sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ focal main'
+
+sudo apt-get update
+
+sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ focal-rc main'
+
+sudo apt-get update
+
+sudo apt-get install kitware-archive-keyring
+
+sudo rm /etc/apt/trusted.gpg.d/kitware.gpg
+
+sudo apt-get install cmake -y 
+
+sudo apt-get update
+
+sudo apt-get dist-upgrade -y
+
+sudo apt-get autoremove -y
+
+sudo apt-get autoclean -y
